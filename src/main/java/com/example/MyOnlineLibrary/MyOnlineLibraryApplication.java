f28a379c2536ec6460be6c809b6178d195ef0f74
kubra kesici <avkubrakesici@gmail.com>
2024-12-10 13:41:17 +0100

    first

---
+++ src/main/java/com/example/MyOnlineLibrary/MyOnlineLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyOnlineLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOnlineLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOnlineLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyOnlineLibrary/MyOnlineLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyOnlineLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOnlineLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
