3fad7a26f9bcf6405deb37eaf654b48b6c90fd44
kubra kesici <avkubrakesici@gmail.com>
2024-12-12 21:06:09 +0100

    3.gün

--- src/main/java/com/example/MyOnlineLibrary/contactmessage/controller/ContactMessageController.java
+++ src/main/java/com/example/MyOnlineLibrary/contactmessage/controller/ContactMessageController.java
@@ -6,10 +6,10 @@ import com.example.MyOnlineLibrary.contactmessage.service.ContactMessageService;
 import com.example.MyOnlineLibrary.payload.response.ResponseMessage;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +18,38 @@ public class ContactMessageController {
 
     private ContactMessageService service;
 
-    @PostMapping("/save")
+    @PostMapping
   ///  @Operation(tags = "Contact Message",summary = "J01")
     public ResponseMessage<ContactMessageResponse> save(@RequestBody @Valid ContactMessageRequest contactMessageRequest) {
         return service.saveMessage(contactMessageRequest);
     }
+
+    @GetMapping
+    public ResponseMessage<ContactMessageResponse> getById(@RequestParam(value = "contactMessageId") UUID contactMessageId){
+        return service.getContactMessageById(contactMessageId);
+    }
+
+    @GetMapping
+    public ResponseMessage<Page<ContactMessageResponse>>  getAllContactMessagesByPages(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sort", defaultValue = "dateTime") String sort,
+            @RequestParam(value = "type", defaultValue = "desc") String type){
+
+        return service.getAllContactMessagesByPages(page, size, sort, type);
+    }
+    @GetMapping("/status")
+    public ResponseMessage<Page<ContactMessageResponse>>  getAllContactMessagesByPages(
+            @RequestParam(value = "q" ,required = false) String q,
+            @RequestParam(value = "status" ,required = false) String status,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sort", defaultValue = "dateTime") String sort,
+            @RequestParam(value = "type", defaultValue = "desc") String type){
+
+        return service.getAllContactMessagesWithStatusByPages(q,status,page,size,sort,type);
+    }
+
+
+
 }
--- src/main/java/com/example/MyOnlineLibrary/contactmessage/service/ContactMessageService.java
+++ src/main/java/com/example/MyOnlineLibrary/contactmessage/service/ContactMessageService.java
@@ -6,10 +6,18 @@ import com.example.MyOnlineLibrary.contactmessage.payload.mapper.ContactMessageM
 import com.example.MyOnlineLibrary.contactmessage.payload.request.ContactMessageRequest;
 import com.example.MyOnlineLibrary.contactmessage.payload.response.ContactMessageResponse;
 import com.example.MyOnlineLibrary.contactmessage.repository.ContactMessageRepository;
+import com.example.MyOnlineLibrary.exception.BadRequestException;
 import com.example.MyOnlineLibrary.payload.response.ResponseMessage;
+import com.example.MyOnlineLibrary.service.helper.PageableHelper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +25,7 @@ public class ContactMessageService {
 
     private final ContactMessageRepository contactMessageRepository;
     private final ContactMessageMapper contactMessageMapper;
+    private final PageableHelper pageableHelper;
 
     public ResponseMessage<ContactMessageResponse> saveMessage(ContactMessageRequest contactMessageRequest) {
       ContactMessage contactMessage = contactMessageMapper.requestToContactMessage(contactMessageRequest);
@@ -27,4 +36,46 @@ public class ContactMessageService {
               .httpStatus(HttpStatus.OK).
               object(contactMessageResponse).build();
   }
+
+    public ResponseMessage<ContactMessageResponse> getContactMessageById(UUID contactMessageId) {
+     ContactMessage contactMessage = contactMessageRepository.getReferenceById(contactMessageId);
+        ContactMessageResponse contactMessageResponse = contactMessageMapper.contactMessageToResponse(contactMessage);
+        return ResponseMessage.<ContactMessageResponse>builder()
+                .httpStatus(HttpStatus.OK).
+                object(contactMessageResponse).build();
+    }
+
+    public ResponseMessage<Page<ContactMessageResponse>> getAllContactMessagesByPages(int page, int size, String sort, String type) {
+      Pageable pageable =  pageableHelper.getPageableWithProperties(page, size, sort, type);
+        Page<ContactMessageResponse> contactMessageResponsePage = contactMessageRepository.findAll(pageable)
+                .map(contactMessageMapper::contactMessageToResponse);
+
+        return ResponseMessage.<Page<ContactMessageResponse>>builder()
+                .httpStatus(HttpStatus.OK)
+                .object(contactMessageResponsePage)
+                .build();
+    }
+
+    public ResponseMessage<Page<ContactMessageResponse>> getAllContactMessagesWithStatusByPages(String q,String requestStatus, int page, int size, String sort, String type ) {
+        Pageable pageable =  pageableHelper.getPageableWithProperties(page, size, sort, type);
+
+        Status status = null;
+        if (Status.READ.getName().equalsIgnoreCase(requestStatus)){
+            status = Status.READ;
+        } else if (Status.UNREAD.getName().equalsIgnoreCase(requestStatus)) {
+            status = Status.UNREAD;
+        } else if (!Objects.isNull(requestStatus) && !requestStatus.equalsIgnoreCase("All")){
+            throw new BadRequestException(String.format("Invalid", requestStatus));
+
+        }
+
+        Page<ContactMessageResponse> contactMessageResponsePage = contactMessageRepository.findAll(pageable)
+                .map(contactMessageMapper::contactMessageToResponse);
+
+        return ResponseMessage.<Page<ContactMessageResponse>>builder()
+                .httpStatus(HttpStatus.OK)
+                .object(contactMessageResponsePage)
+                .build();
+    }
+
 }
